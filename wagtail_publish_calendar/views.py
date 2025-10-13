31eb806be1056cc0856e20fc66a03cb56b8bcecb
Jawad Khan <jawadkhan444@gmail.com>
2025-10-13 14:22:32 +0500

    feat: Added support for general publishing object

--- wagtail_publish_calendar/views.py
+++ wagtail_publish_calendar/views.py
@@ -1,11 +1,11 @@
 import json
-from itertools import chain
 
+from django.apps import apps
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
-from wagtail.models import Page, Revision
+from wagtail.models import Page, Revision, DraftStateMixin
 
 from .forms import ScheduleForm
 
@@ -16,30 +16,50 @@ def calendar_view(request):
     """
     form = ScheduleForm()
     context = {"form": form, "page_title": "Page Scheduler"}
-    return render(request, "wagtail_publish_calendar/calendar.html", context)
+    return render(
+        request,
+        "wagtail_publish_calendar/calendar.html",
+        context
+    )
 
 
 def get_page_schedule_dates(request):
     """
     Creates event data with descriptive titles and color codes.
     """
-    expired_pages = Page.objects.filter(live=True, expire_at__gt=timezone.now()).order_by("expire_at")
-    revisions = Revision.objects.filter(approved_go_live_at__gt=timezone.now()).order_by("approved_go_live_at")
+    models = [Page]
+    models += [
+        model
+        for model in apps.get_models()
+        if issubclass(model, DraftStateMixin) and not issubclass(model, Page)
+    ]
+
+    # 1. get all expired objects with live = True
+    expired_qs = []
+    for model in models:
+        expired_qs += [
+            model.objects.filter(live=True, expire_at__gt=timezone.now()).order_by(
+                "expire_at"
+            )
+        ]
 
     events = []
-    for event in chain(expired_pages, revisions):
-        if getattr(event, "expire_at", None):
-            events.append(get_expire_event(event))
-        if getattr(event, "approved_go_live_at", None):
-            events.append(get_publish_event(event))
+    for queryset in expired_qs:
+        for obj in queryset:
+            events.append(get_expire_event(obj))
+
+    revisions = Revision.objects.filter(
+        approved_go_live_at__gt=timezone.now()).order_by("approved_go_live_at")
+    for event in revisions:
+        events.append(get_publish_event(event))
 
     return JsonResponse(events, safe=False)
 
 
 def get_publish_event(event):
     return {
-        "id": f"{event.object_id}-start",
-        "title": f"{event.content.get('title')} (Go-live)",  # <-- COMBINED TITLE
+        "id": f"{event.id}-start",
+        "title": f"{event.object_str} (Go-live)",  # <-- COMBINED TITLE
         "start": event.approved_go_live_at.isoformat(),
         "color": "#008352",
         "extendedProps": {"type": "start"},
@@ -47,9 +67,10 @@ def get_publish_event(event):
 
 
 def get_expire_event(event):
+    revision = event.latest_revision
     return {
-        "id": f"{event.id}-end",
-        "title": f"{event.title} (Expire)",  # <-- COMBINED TITLE
+        "id": f"{revision.id}-end",
+        "title": f"{revision.object_str} (Expire)",  # <-- COMBINED TITLE
         "start": event.expire_at.isoformat(),
         "color": "#cd4444",
         "extendedProps": {"type": "end"},
@@ -66,20 +87,22 @@ def update_page_schedule_date(request):
 
     try:
         data = json.loads(request.body)
-        page = Page.objects.get(id=data.get("page_id"))
+        revision = Revision.objects.get(id=data.get("page_id"))
 
         # A null or empty string from the frontend means the date should be cleared
         go_live_str = data.get("go_live_at")
         expire_str = data.get("expire_at")
 
-        revision = page.latest_revision
-        revision.approved_go_live_at = parse_datetime(go_live_str) if go_live_str else None
+        revision.content_object.expire_at = parse_datetime(
+            expire_str) if expire_str else None
+        revision.content_object.save()
+
+        revision.approved_go_live_at = parse_datetime(
+            go_live_str) if go_live_str else None
         revision.save()
 
-        page.expire_at = parse_datetime(expire_str) if expire_str else None
-        page.save()
-
-        return JsonResponse({"status": "ok", "message": f"Schedule for '{page.title}' updated."})
+        return JsonResponse({"status": "ok",
+                             "message": f"Schedule for '{revision.object_str}' updated."})
 
     except Page.DoesNotExist:
         return JsonResponse({"error": "Page not found"}, status=404)
